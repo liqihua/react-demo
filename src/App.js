c0272a820c84c7d4b207bc53fe63390512465142
qihua.li <qihua.li@wehotelglobal.com>
2019-05-31 18:08:47 +0800

    liqihua

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: '',
+      list: []
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        123{this.props.name}
+        <div>
+          <input value={this.state.inputValue} onChange={this.handleInputChange.bind(this)}/>
+          <button onClick={this.handleBtnClick.bind(this)}>提交</button>
+        </div>
+        <ul>
+          {
+            this.state.list.map((item,index) => {
+              return (
+                <li key={index}>{item}</li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+
+  handleBtnClick() {
+    this.setState({
+      list: [...this.state.list,this.state.inputValue],
+      inputValue: ''
+    })
+  }
+  
 }
 
 export default App;
