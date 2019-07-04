df2b3ebd2fb50b7ca71642ec5764df7ff4bb95e3
qihua.li <qihua.li@wehotelglobal.com>
2019-07-04 17:42:07 +0800

    liqihua

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React from 'react'
 import List from './pages/list/List'
 import { Provider } from 'react-redux'
 import store from './store'
+import {BrowserRouter, Route} from 'react-router-dom'
 
 class App extends React.Component {
     render() {
         return (
             <Provider store={store}>
-                <List/>
+                <BrowserRouter>
+                    <Route path='/' exact component={List}></Route>
+                    <Route path='/item' exact render={() => <div>item</div>}></Route>
+                </BrowserRouter>
             </Provider>
         );
     }
